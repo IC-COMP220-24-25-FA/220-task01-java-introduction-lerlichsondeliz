f998d887042848dba9ceec204fc8ed79cfbc4cef
lerlichsondeliz <lerlichsondeliz@ithaca.edu>
2024-08-28 20:00:27 -0400

    added code for findLastLongest

--- src/main/java/edu/ithaca/dragon/practice/FunctionPractice.java
+++ src/main/java/edu/ithaca/dragon/practice/FunctionPractice.java
@@ -69,7 +69,18 @@ public class FunctionPractice {
      * If the largest number occurs more than once, return the index of the last occurence.
      */
     public static int findLastLargest(List<Integer> numbers){
-        throw new RuntimeException("Not Implemented");
+        if (numbers.isEmpty()){
+            return -1;
+        }
+        int largest = numbers.get(0);
+        int index = 0;
+        for (int i = 1; i < numbers.size(); i++) {
+            if (numbers.get(i) >= largest) {
+                largest = numbers.get(i);
+                index = i;
+            }
+        }
+        return index;
     }
 
     /**
