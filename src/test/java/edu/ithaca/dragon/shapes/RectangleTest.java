ce3352d063560413de2bc28ce29b8cdfaa158e1e
lerlichsondeliz <lerlichsondeliz@ithaca.edu>
2024-08-29 23:34:19 -0400

    added tests for error and for doubleSize

--- src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
+++ src/test/java/edu/ithaca/dragon/shapes/RectangleTest.java
@@ -19,5 +19,32 @@ public class RectangleTest {
 
     myRectangle = new Rectangle(3.5, 2);
     assertEquals(7, myRectangle.calcArea(),0.001);
+
+    
     }
+    @Test
+    public void doubleSize(){
+    Rectangle myRectangle = new Rectangle(1, 1);
+    myRectangle.doubleSize();
+    assertEquals(4, myRectangle.calcArea(), 0.001);
+
+    myRectangle = new Rectangle(2, 3);
+    myRectangle.doubleSize();
+    assertEquals(24, myRectangle.calcArea(), 0.001);
+
+    myRectangle = new Rectangle(4, 3);
+    myRectangle.doubleSize();
+    assertEquals(48, myRectangle.calcArea(), 0.001);
+    }
+
+
+    @Test
+    public void TestConstructorError(){
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(0, 1));
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(1, 0));
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(-1, 1));
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(10, -1));
+    }
+
+    
 }
