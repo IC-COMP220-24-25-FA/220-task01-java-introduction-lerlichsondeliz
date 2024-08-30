023ad4c0f73589f206843430f0fcb077230e98f7
lerlichsondeliz <lerlichsondeliz@ithaca.edu>
2024-08-29 23:23:26 -0400

    added code for public Rectangle

--- src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
@@ -1,10 +1,15 @@
 package edu.ithaca.dragon.shapes;
 
 public class Rectangle {
-    
+    private double length;
+    private double width;
 
     public Rectangle(double length, double width){
-        throw new RuntimeException("Not implemented yet");
+        if (length <= 0 || width <= 0){
+            throw new IllegalArgumentException("Both length and with must be positive");
+        }
+        this.length = length;
+        this.width = width;
     }
 
     public double calcArea(){
